ed54ff4f93d1d331aefdc2be9152c7c20f8cc5e0
rodgrac <rodneygracian@gmail.com>
2019-11-18 22:25:45 -0500

    Created gabor filter bank generator

--- src/gabor_main.py
+++ src/gabor_main.py
@@ -7,15 +7,12 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-lam = np.arange(0.24, 0.12, -0.02)
-theta_res = np.pi / 8
-theta_count = 9
 
-fig1, axs1 = plt.subplots(np.size(lam), theta_count + 1, sharex='col', sharey='row',
-                          gridspec_kw={'hspace': 0, 'wspace': 0})
-fig1.suptitle("Gabor kernels - Orientation against Frequency")
-fig2, axs2 = plt.subplots(np.size(lam), theta_count + 1, sharex='col', sharey='row',
-                          gridspec_kw={'hspace': 0, 'wspace': 0})
+def createbankParams(f_max, f_n, theta_div):
+    f = np.geomspace(f_max / 2 ** (f_n - 1), f_max, num=f_n, dtype=float)
+    thet = np.arange(theta_div + 1) * 2 * np.pi / theta_div
+
+    return f, thet
 
 
 # f: frequency; theta: orientation; sigma: gaussian envelope
@@ -27,9 +24,9 @@ def createGaborKernel(window_size, f, theta, sigma):
     yy_dot = -xx * np.sin(theta) + yy * np.cos(theta)
 
     gaussian = np.exp((-0.5 * f ** 2 * (xx_dot ** 2 + yy_dot ** 2)) / sigma ** 2)
-    gaussian = (f ** 2 / (2 * np.pi * sigma ** 2)) * gaussian
+    gaussian = (f ** 2 / (np.pi * sigma ** 2)) * gaussian
 
-    harmonic = np.cos(f * xx_dot) * np.exp(sigma ** 2 / 2)
+    harmonic = np.cos(2 * np.pi * f * xx_dot) * np.exp(sigma ** 2 / 2)
 
     gabor = gaussian * harmonic
 
@@ -38,17 +35,27 @@ def createGaborKernel(window_size, f, theta, sigma):
 
 if __name__ == "__main__":
     img = cv2.imread('../res/zebra.jpeg')
+    cv2.imshow('input', img)
     img_g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    for i in range(0, np.size(lam)):
-        for j in range(0, theta_count + 1):
-            g_kernel = createGaborKernel(31, 1 / lam[i], theta_res * j, 6.0)
+
+    freq, theta = createbankParams(np.sqrt(2) / 4, 5, 8)
+
+    fig1, axs1 = plt.subplots(np.size(freq), np.size(theta), sharex='col', sharey='row',
+                              gridspec_kw={'hspace': 0, 'wspace': 0})
+    fig1.suptitle("Gabor kernels - Orientation against Frequency")
+    fig2, axs2 = plt.subplots(np.size(freq), np.size(theta), sharex='col', sharey='row',
+                              gridspec_kw={'hspace': 0, 'wspace': 0})
+
+    for i in range(np.size(freq)):
+        for j in range(np.size(theta)):
+            g_kernel = createGaborKernel(31, freq[i], theta[j], 1.0)
             # g_kernel = cv2.getGaborKernel((31, 31), 2.0, theta_res, lam[i], 1, 0, ktype=cv2.CV_32F)
-            out = cv2.filter2D(img_g, cv2.CV_8UC3, g_kernel)
+            out = cv2.filter2D(img_g, cv2.CV_32F, g_kernel)
             kh, kw = g_kernel.shape[:2]
             ih, iw = img_g.shape[:2]
             g_kernel_resized = cv2.resize(g_kernel, (3 * kw, 3 * kh), interpolation=cv2.INTER_CUBIC)
             axs1[i, j].imshow(g_kernel_resized, cmap='gray')
-            axs1[i, j].set(xlabel=np.str(j) + "π/8", ylabel=lam[i])
+            axs1[i, j].set(xlabel=(theta[j] / np.pi) * 180, ylabel=freq[i])
             axs1[i, j].set_xticklabels([])
             axs1[i, j].set_yticklabels([])
             axs1[i, j].label_outer()
@@ -57,5 +64,6 @@ if __name__ == "__main__":
             axs2[i, j].set_xticklabels([])
             axs2[i, j].set_yticklabels([])
             axs2[i, j].label_outer()
+    cv2.waitKey(1)
 
     plt.show()
