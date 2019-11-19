683124292792f7d9f989d8c50fccb1dc9f75beca
rodgrac <rodneygracian@gmail.com>
2019-11-19 15:40:49 -0500

    Made minor changes in the kernel definition and the bank parameters for optimized bank selection

--- src/gabor_main.py
+++ src/gabor_main.py
@@ -10,23 +10,23 @@ from matplotlib import pyplot as plt
 
 def createbankParams(f_max, f_n, theta_div):
     f = np.geomspace(f_max / 2 ** (f_n - 1), f_max, num=f_n, dtype=float)
-    thet = np.arange(theta_div + 1) * 2 * np.pi / theta_div
+    thet = np.arange(theta_div) * 2 * np.pi / theta_div
 
     return f, thet
 
 
 # f: frequency; theta: orientation; sigma: gaussian envelope
-def createGaborKernel(window_size, f, theta, sigma):
+def createGaborKernel(window_size, f, theta, sigma_x, sigma_y):
     mid_val = int((window_size - 1) / 2)
     [xx, yy] = np.meshgrid(range(-mid_val, mid_val + 1), range(-mid_val, mid_val + 1))
 
     xx_dot = xx * np.cos(theta) + yy * np.sin(theta)
     yy_dot = -xx * np.sin(theta) + yy * np.cos(theta)
 
-    gaussian = np.exp((-0.5 * f ** 2 * (xx_dot ** 2 + yy_dot ** 2)) / sigma ** 2)
-    gaussian = (f ** 2 / (np.pi * sigma ** 2)) * gaussian
+    gaussian = np.exp(-0.5 * f ** 2 * ((xx_dot ** 2 / sigma_x ** 2) + (yy_dot ** 2 / sigma_y ** 2)))
+    gaussian = (f ** 2 / (np.pi * sigma_x * sigma_y)) * gaussian
 
-    harmonic = np.cos(2 * np.pi * f * xx_dot) * np.exp(sigma ** 2 / 2)
+    harmonic = np.cos(2 * np.pi * f * xx_dot)
 
     gabor = gaussian * harmonic
 
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     cv2.imshow('input', img)
     img_g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    freq, theta = createbankParams(np.sqrt(2) / 4, 5, 8)
+    freq, theta = createbankParams(1 / (2 + (2 * np.sqrt(np.log(2)) / np.pi)), 4, 4)
 
     fig1, axs1 = plt.subplots(np.size(freq), np.size(theta), sharex='col', sharey='row',
                               gridspec_kw={'hspace': 0, 'wspace': 0})
@@ -48,7 +48,7 @@ if __name__ == "__main__":
 
     for i in range(np.size(freq)):
         for j in range(np.size(theta)):
-            g_kernel = createGaborKernel(31, freq[i], theta[j], 1.0)
+            g_kernel = createGaborKernel(31, freq[i], theta[j], 0.5, 0.5)
             # g_kernel = cv2.getGaborKernel((31, 31), 2.0, theta_res, lam[i], 1, 0, ktype=cv2.CV_32F)
             out = cv2.filter2D(img_g, cv2.CV_32F, g_kernel)
             kh, kw = g_kernel.shape[:2]
