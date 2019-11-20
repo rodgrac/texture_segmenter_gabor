9eb88d2b31b97c56c0e56cd7ae69375334c4b117
rodgrac <rodneygracian@gmail.com>
2019-11-19 23:29:01 -0500

    Added changes for texture enhancement using the filter bank

--- src/gabor_main.py
+++ src/gabor_main.py
@@ -23,7 +23,7 @@ def createGaborKernel(window_size, f, theta, sigma_x, sigma_y):
     xx_dot = xx * np.cos(theta) + yy * np.sin(theta)
     yy_dot = -xx * np.sin(theta) + yy * np.cos(theta)
 
-    gaussian = np.exp(-0.5 * f ** 2 * ((xx_dot ** 2 / sigma_x ** 2) + (yy_dot ** 2 / sigma_y ** 2)))
+    gaussian = np.exp(-f ** 2 * ((xx_dot ** 2 / sigma_x ** 2) + (yy_dot ** 2 / sigma_y ** 2)))
     gaussian = (f ** 2 / (np.pi * sigma_x * sigma_y)) * gaussian
 
     harmonic = np.cos(2 * np.pi * f * xx_dot)
@@ -34,11 +34,11 @@ def createGaborKernel(window_size, f, theta, sigma_x, sigma_y):
 
 
 if __name__ == "__main__":
-    img = cv2.imread('../res/zebra.jpeg')
-    cv2.imshow('input', img)
-    img_g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_g = cv2.imread('../res/zebra.jpeg')
+    cv2.imshow('input', img_g)
+    # img_g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    freq, theta = createbankParams(1 / (2 + (2 * np.sqrt(np.log(2)) / np.pi)), 4, 4)
+    freq, theta = createbankParams(1 / (2 + (2 * np.sqrt(np.log(2)) / np.pi)), 4, 8)
 
     fig1, axs1 = plt.subplots(np.size(freq), np.size(theta), sharex='col', sharey='row',
                               gridspec_kw={'hspace': 0, 'wspace': 0})
@@ -46,11 +46,14 @@ if __name__ == "__main__":
     fig2, axs2 = plt.subplots(np.size(freq), np.size(theta), sharex='col', sharey='row',
                               gridspec_kw={'hspace': 0, 'wspace': 0})
 
+    accum = np.zeros_like(img_g)
     for i in range(np.size(freq)):
         for j in range(np.size(theta)):
             g_kernel = createGaborKernel(31, freq[i], theta[j], 0.5, 0.5)
-            # g_kernel = cv2.getGaborKernel((31, 31), 2.0, theta_res, lam[i], 1, 0, ktype=cv2.CV_32F)
-            out = cv2.filter2D(img_g, cv2.CV_32F, g_kernel)
+            # g_kernel = cv2.getGaborKernel((31, 31), 4.0, theta[j], 10.0, 0.5, 0, ktype=cv2.CV_32F)
+            g_kernel /= 2 * g_kernel.sum()
+            out = cv2.filter2D(img_g, cv2.CV_8UC3, g_kernel)
+            np.maximum(accum, out, accum)
             kh, kw = g_kernel.shape[:2]
             ih, iw = img_g.shape[:2]
             g_kernel_resized = cv2.resize(g_kernel, (3 * kw, 3 * kh), interpolation=cv2.INTER_CUBIC)
@@ -64,6 +67,9 @@ if __name__ == "__main__":
             axs2[i, j].set_xticklabels([])
             axs2[i, j].set_yticklabels([])
             axs2[i, j].label_outer()
-    cv2.waitKey(1)
+
+    cv2.imshow('Out', accum)
+    cv2.waitKey(0)
 
     plt.show()
+    cv2.destroyAllWindows()
