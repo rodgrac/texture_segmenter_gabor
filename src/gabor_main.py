67a23626079b694d51d9015c687d27c230613612
rodgrac <rodneygracian@gmail.com>
2019-11-22 17:45:49 -0500

    Made a few changes in normalization and added logic for feature/background colors

--- src/gabor_main.py
+++ src/gabor_main.py
@@ -7,7 +7,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-color = np.float32([[1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 1, 0]])
+color = np.float32([[1, 0, 0], [0, 1, 0]])
 
 
 def createbankParams(f_max, f_n, theta_div):
@@ -59,16 +59,19 @@ def reduced_image(img_bank):
     reduced_fsp_bank = []
     img_bank_mat = np.asarray(img_bank)
     t_shape = img_bank_mat.shape
-    img_tensor = np.reshape(img_bank_mat, (t_shape[0], t_shape[1] * t_shape[2], t_shape[3]))
+    img_tensor = np.reshape(img_bank_mat, (t_shape[0], t_shape[1] * t_shape[2], t_shape[3])).astype('float32')
     img_acc = np.zeros((t_shape[1], t_shape[2], t_shape[3]))
 
     for i in range(3):
-        tensor_cov = np.cov(img_tensor[:, :, i])
+        # temp = ((img_tensor[:, :, i].T - np.mean(img_tensor[:, :, i].T, axis=0)) / (
+        #     np.std(img_tensor[:, :, i].T, axis=0))).T
+        temp = img_tensor[:, :, i]
+        tensor_cov = np.cov(temp)
         [U, D, V] = np.linalg.svd(tensor_cov)
         t = np.where(np.cumsum(D) / np.sum(D) > 0.99)[0][0]
         reduced_ker = U[:, :t + 1] / D[:t + 1]
 
-        reduced_fsp = np.dot(reduced_ker.T, img_tensor[:, :, i])
+        reduced_fsp = np.dot(reduced_ker.T, temp)
 
         reduced_fsp_bank.extend(reduced_fsp)
 
@@ -77,12 +80,12 @@ def reduced_image(img_bank):
     return reduced_fsp_bank, img_acc
 
 
-def gaussian_smoothing(img_bank, shape):
-    gauss = cv2.getGaussianKernel(ksize=5, sigma=1.0)
-
+def gaussian_smoothing(img_bank, shape, ksize, sigma):
+    gauss = cv2.getGaussianKernel(ksize, sigma)
     for i in range(img_bank.shape[0]):
         img = np.reshape(img_bank[i], (-1, shape[1]))
-        img_bank[i] = cv2.filter2D(img, -1, gauss).flatten()
+        temp = cv2.filter2D(img, -1, gauss)
+        img_bank[i] = temp.flatten()
 
     return img_bank
 
@@ -126,32 +129,33 @@ def plotFilteredImages(f_img, freq, theta):
 
 
 if __name__ == "__main__":
-    img = cv2.imread('../res/zebra.jpg')
+    img = cv2.imread('../res/faces.jpg')
     img_s = img.shape
 
-    freq, theta = createbankParams(1 / (2 + (2 * np.sqrt(np.log(2)) / np.pi)), 4, 8)
+    freq, theta = createbankParams(1 / (2 + (2 * np.sqrt(np.log(2)) / np.pi)), 5, 8)
 
     filter_bank = createFilterBank(31, freq, theta, 0.5, 0.5)
     out, img_bank = filterImage(img, filter_bank)
 
     red_bank, fs_img = reduced_image(img_bank)
     output = np.asarray(red_bank)
-    output_n = ((output.T - np.min(output.T, axis=0)) / (np.max(output.T, axis=0) - np.min(output.T, axis=0))).T
+    output_n = ((output.T - np.min(output.T, axis=0)) / ((np.max(output.T, axis=0)) - (np.min(output.T, axis=0)))).T
     output_e = np.tanh(output_n)
-    output_ne = ((output_e.T - np.min(output_e.T, axis=0))
-                 / (np.max(output_e.T, axis=0) - np.min(output_e.T, axis=0))).T
+    output_ne = ((output_e.T - np.min(output_e.T, axis=0)) / (
+            (np.max(output_e.T, axis=0)) - (np.min(output_e.T, axis=0)))).T
 
-    output_g = gaussian_smoothing(output_ne, img_s)
-
-    output_ng = ((output_g.T - np.min(output_g.T, axis=0))
-                 / (np.max(output_g.T, axis=0) - np.min(output_g.T, axis=0)))
+    output_g = gaussian_smoothing(output_ne, img_s, 5, 2)
+    output_ng = ((output_g.T - np.min(output_g.T, axis=0)) / (
+            (np.max(output_g.T, axis=0)) - (np.min(output_g.T, axis=0))))
 
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    K = 2
-    ret, label, center = cv2.kmeans(np.float32(output_ng), K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    ret, label, center = cv2.kmeans(np.float32(output_ng), 2, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
     # color = np.random.randn(K, 3)
-    res = color[label.flatten()]
+    if (sum(label) / label.size) > 0.5:
+        res = color[label.flatten()]
+    else:
+        res = color[~label.flatten()]
     res2 = res.reshape(img_s)
 
     cv2.imshow('Input', img)
